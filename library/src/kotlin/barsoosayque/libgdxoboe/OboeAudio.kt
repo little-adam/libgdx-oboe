59984e3917a984e71bd8b1b2a8001b4263db8bef
Matthew Yang <matthew.yang@xuexue365.com>
2021-06-30 13:36:30 +0800

    Remove unused music list and sound list

--- library/src/kotlin/barsoosayque/libgdxoboe/OboeAudio.kt
+++ library/src/kotlin/barsoosayque/libgdxoboe/OboeAudio.kt
@@ -10,15 +10,12 @@ import com.badlogic.gdx.audio.Sound
 import com.badlogic.gdx.backends.android.AndroidAudio
 import com.badlogic.gdx.backends.android.AndroidMusic
 import com.badlogic.gdx.files.FileHandle
-import com.badlogic.gdx.utils.Disposable
 
 /** [Audio] implementation which utilize [OboeMusic] and [OboeSound] */
 // TODO: delegate errors from c++ to GdxRuntimeException
 class OboeAudio(private val assetManager: AssetManager) : AndroidAudio {
     private var audioEngine: NativeAudioEngine = NativeAudioEngine()
     private val audioDevicesList: MutableList<AudioDevice> = mutableListOf()
-    private val musicList: MutableList<Music> = mutableListOf()
-    private val soundsList: MutableList<Sound> = mutableListOf()
 
     init {
         System.loadLibrary("libgdx-oboe")
@@ -45,21 +42,17 @@ class OboeAudio(private val assetManager: AssetManager) : AndroidAudio {
         disposeEngine()
 
         audioDevicesList.onEach { it.dispose() }.clear()
-        musicList.onEach { it.dispose() }.clear()
-        soundsList.onEach { it.dispose() }.clear()
     }
 
     override fun newMusic(file: FileHandle): Music = when (file.type()) {
         Files.FileType.Internal -> createMusicFromAsset(assetManager, file.path())
         else -> createMusicFromPath(file.file().path)
     }.let(::OboeMusic)
-            .also { musicList.add(it) }
 
     override fun newSound(file: FileHandle): Sound = when (file.type()) {
         Files.FileType.Internal -> createSoundpoolFromAsset(assetManager, file.path())
         else -> createSoundpoolFromPath(file.file().path)
     }.let(::OboeSound)
-            .also { soundsList.add(it) }
 
     override fun newAudioDevice(samplingRate: Int, isMono: Boolean): AudioDevice =
             createAudioEngine(samplingRate, isMono)
